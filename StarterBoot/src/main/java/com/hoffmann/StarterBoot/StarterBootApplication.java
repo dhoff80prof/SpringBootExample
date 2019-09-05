2dd5ddf704617895d3557f292bdaf8c3f6e10a94
dhoff80prof <davhoffs80@gmail.com>
2019-09-05 18:27:05 -0400

    Added the Project
    Added the SpringBoot

---
+++ StarterBoot/src/main/java/com/hoffmann/StarterBoot/StarterBootApplication.java
@@ -0,0 +1,13 @@
+package com.hoffmann.StarterBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterBootApplication.class, args);
+	}
+
+}
